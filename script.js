2335745b69cf63558b8dd4129535cd1b2604c0d4
LcEduardo <lucasproject777@gmail.com>
2024-12-05 15:50:48 -0300

    responsividade teste inicial

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Aqui é o famoso JS ? Justin Bieber?
+
+function ajustarConteudo() {
+    const larguraTela = window.innerWidth;
+    const conteudo = document.getElementById("noimg");
+
+    const section = document.querySelector("section.btn");
+    const caixaTexo = document.getElementById("caixaTexto");
+    const botaoDelete = document.getElementById("delete");
+
+    if (larguraTela < 431) {
+        section.insertBefore(caixaTexo, botaoDelete);
+
+    }
+}
+
+ajustarConteudo();
+
+window.onresize = ajustarConteudo;
