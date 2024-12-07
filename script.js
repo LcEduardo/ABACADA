1166ae1890d900fe67ccc29fd570f09fda6a8977
LcEduardo <lucasproject777@gmail.com>
2024-12-07 16:17:15 -0300

    Mudando loayout

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // Aqui é o famoso JS ? Justin Bieber?
 
-function ajustarConteudo() {
-    const larguraTela = window.innerWidth;
-    const conteudo = document.getElementById("noimg");
-
-    const section = document.querySelector("section.btn");
-    const caixaTexo = document.getElementById("caixaTexto");
-    const botaoDelete = document.getElementById("delete");
-
-    if (larguraTela > 431) {
-        section.insertBefore(caixaTexo, botaoDelete);
-
-    }
-}
-
-ajustarConteudo();
-
-window.onresize = ajustarConteudo;
-
 const popup = document.querySelector("#popup");
 const modal = document.querySelector("dialog");
 const fechar = document.querySelector("#ok");
