7f56061ae93450dd02bbde03bc1f9212d8568aba
LcEduardo <lucasproject777@gmail.com>
2024-12-09 11:46:25 -0300

    colocando os botões para funcionar

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ popup.onclick = function () {
 ok.onclick = function () {
     modal.close();
 };
+
+// Colocando so botões para funcionar bb;
+
+// Seleciona todos os btns que da classe .abcd
+btns = document.querySelectorAll(".abcd");
+txtView = document.getElementById("txtView");
+
+// Especifica a mesma função para cada botão clicado
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+
+        // textContent verifica o campo de texto do botão clicado
+        alert(`Clicou no botão: ${btn.textContent}`)
+    })
+})
