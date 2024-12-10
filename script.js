3cc018e6bf9702d8fc1b7358877a1ee0a77583b8
LcEduardo <lucasproject777@gmail.com>
2024-12-10 11:58:09 -0300

    Arrumando estouro de linha wrap

--- script.js
+++ script.js
@@ -12,12 +12,8 @@ ok.onclick = function () {
     modal.close();
 };
 
-// Colocando so botões para funcionar bb;
-
-// Seleciona todos os btns que da classe .abcd
 const btns = document.querySelectorAll(".abcd");
 const txtView = document.getElementById("txtView");
-const botaodelete = document.querySelector("#delete");
 
 let char = [];
 
@@ -28,7 +24,6 @@ btns.forEach(function(btn) {
         let texto = "";
 
         for(const i in char){
-            
             texto += char[i];
             
         }
@@ -38,23 +33,22 @@ btns.forEach(function(btn) {
     });
 });
 
-botaodelete.addEventListener("click", function() { //aqui pode usar o addEventLSITENER direto
-    char = []; // Tive que usar o Char aqui novamente para podere "zerar" ele assim pq se não ele continua armazenando o valor de cima
-    txtView.innerText = ""; //Aqui nesse caso ao inves de atribuir algum valor a gente passa valor zero
+const botaodelete = document.querySelector("#delete");
+
+botaodelete.addEventListener("click", function() { 
+    char = [];
+    txtView.innerText = "";
 });
 
 const botaocheck = document.querySelector("#check");
 const popcheck = document.querySelector("#popcheck");
 const botaofechar2 = document.querySelector("#fechar1");
+const txtView2 = document.getElementById("txtView2");
+
 
 botaocheck.onclick = function () {
-    popcheck.showModal(); //popchek é o modal
-    txtView2.innerHTML = `Parabéns! Você formou a palavra:<br>${char.join("")} <br>`; //tive que usar o innerhtml para conseguir pular linha mas poderia ser innertext ou textcontent
-    //O textContente é para poder adicionar no pop sem ele não tem como usar a função texto q vc criou 
-    // pai aqui eu so trouxe o array e sei o join que é uma função para juntar as palvras 
-    //vsf trampo do caralho #desisto 
-    //ferias
-    //vsf dnv
+    popcheck.showModal(); 
+    txtView2.innerHTML = `${char.join("")}`;
 
 };
 
